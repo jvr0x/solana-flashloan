a1ac4de6a3f6d8efe1043a5845cdb9eec3131393
jvr0x <javier@cosmwasmdev.com>
2025-11-26 15:19:21 +0000

    chore: initial commit

---
+++ programs/flash_loan/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("G5fiBBkNj5BtaDjo9ZV3nCUtd7mDSc5KhhPDweVzFRW6");
+
+#[program]
+pub mod flash_loan {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
